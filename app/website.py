3826a0969a2c2f4d3935bcd999c37a753ab9b85b
one-eyed-bat <mikolashuzaurus@gmail.com>
2023-12-21 23:25:58 +0000

    Added the flask backend to start prototyping building on fleek.

--- app/website.py
+++ app/website.py
@@ -7,3 +7,7 @@ app = Flask(__name__)
 @app.route("/")
 def index(name=None):
     return render_template('index.html', name=name)
+
+@app.route("/about/")
+def about(name=None):
+    return render_template('about.html', name=name)
